3899644b78aba3143e5739bcb50f54a7e2cc08d3
ilanmelki <ilanmelki8@gmail.com>
2019-06-21 16:08:33 +0200

    boss vie et degats premiere version

---
+++ Doom boss/boss.js
@@ -0,0 +1,69 @@
+var affichagevieplayer=document.getElementById('affichagevieplayer');
+var affichagevieboss=document.getElementById('affichagevieboss');
+var boss_vie = 95;
+var boss_degat = 10;
+var player_vie = 100;
+
+
+
+function bossvie() 
+{
+	if(boss_vie<=0)
+	{
+		console.log("boss mort");
+		
+		affichagevieboss.innerHTML = boss_vie;
+		boss_vie = 100;
+	}
+	else
+	{
+        affichagevieboss.innerHTML = boss_vie;
+        boss_vie = boss_vie-5;
+
+
+    }
+    
+
+   
+
+
+}
+
+
+function bossdegat()
+{
+    
+
+	
+    	
+    if(player_vie<=0)
+    {
+    
+        
+        console.log("you died");
+        affichagevieplayer.innerHTML = player_vie;
+    	player_vie = 100;
+    }
+
+    else
+    
+    {
+    	
+    	affichagevieplayer.innerHTML = player_vie;
+        player_vie = player_vie-20;
+    }
+
+	
+    
+}
+
+    setInterval("bossdegat()",2000);
+
+
+
+  
+	
+	
+    
+
+
