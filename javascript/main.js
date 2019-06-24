c35d7193c1d0d8504d2b800ed514e47f6475dcd8
iNoSec2 <piereaupjpeje@gmail.com>
2019-06-24 12:06:36 +0200

    reglage tout les compteurs et leurs affichages

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,35 @@
 
+function start(){
+    myImg.onclick=mechantvie;
+    setInterval("mechantdegat()",1000);
+setInterval("zoomin()",800);
+cliqueMechant2()
+    
+}
+
+//armes icones
+
+var iconeArme=document.getElementById("iconeArme")
+
+function affichageArme(){
+    iconeArme.src="../images/armes/1ere arme.png"
+    iconeArme.style.width="150px"; 
+    iconeArme.style.height="70px"; 
+
+}
+affichageArme()
 //PLAYER
 
 var mechant2 = document.getElementById('mechant2');
 var gain = document.getElementById('playerGain');
 var score = document.getElementById('playerScore');
-var comptGain = 0;
-var comptScore = 0;
+var comptGain = -1;
+var comptScore = -1;
 var VieMechant =  100;
 var mechantMort = 10;
 
 function cliqueMechant2(){
 	comptScore++;
-	console.log('score = ' +comptScore);
 	score.innerHTML = comptScore;
 	/*--------------------Score pour chaqueClique--------------------------*/
 
@@ -19,17 +37,12 @@ function cliqueMechant2(){
 	if(VieMechant >= 5){//le vie ennemi 100%
         VieMechant = VieMechant-5;
 		
-		console.log('VieMechant = ' +VieMechant);
 		comptGain++;
-		console.log('gain = ' +comptGain)
 		gain.innerHTML = comptGain;
 	}/*-------------------Gain apres avoir tuer le mechant---------------*/
-	else if (VieMechant == 0){
-		console.log('comptGain = comptGain + mechantMort = ' +comptGain);
-		console.log('score = ' +comptScore);
+	else if (VieMechant == 5){
 		gain.innerHTML = comptGain;
 		VieMechant = 100;
-		console.log ('VieMechant = ' +VieMechant);
 	}/*-----------------------------------------------------------------*/
 	/*--------------------Pour afficher les equipements-------------*/
 	if(comptGain >= acheterPistolet ){
@@ -46,7 +59,6 @@ function cliqueMechant2(){
 	}
 }
 
-
 //Mechant normal
 
 var affichagevieplayer=document.getElementById('playerVie');
@@ -59,7 +71,6 @@ function mechantvie()
 	
 	if(VieMechant<5)
 	{
-
         reinit()
         VieMechant = 100;
         affichageviemechant.innerHTML = VieMechant + " %"; 
@@ -75,54 +86,63 @@ function mechantvie()
 
 function restart(){
     mort_img.src="";
-    reinit();
+    reinit2();
 }
 
-
+var nbVie=document.getElementById("nbVie");
+nbrVie=3;
+nbVie.innerHTML=nbrVie;
 //Quand on meurt
 
 var mort_img = document.getElementById("mort_img")
 function mechantdegat()
 {
+    
     if(player_vie==0)
     {
         
         affichagevieplayer.innerHTML = player_vie+ " %";;
-        player_vie = 100;
+        player_vie = 0;
         mort_img.src="../images/mort ou fin/mort_nous.gif";
         myImg.style.display="none";
         mort_img.onclick=restart;
+        
+        
         
     }
     else
     {
     	affichagevieplayer.innerHTML = player_vie+ " %";;
         player_vie = player_vie-5;
+        
+
+
     }
 }
-setInterval("mechantdegat()",500);
 
 
 //avancement mechant
 
 var myImg = document.getElementById("mechantImg");
 function zoomin(){
+
     var currHeight = myImg.clientHeight;
     var currWidth = myImg.clientWidth;
     var debHeight = 252;
     var debWidth = 200;
 		if(currHeight >= 500){ 
             reinit()
+        
 		} else{
-            myImg.style.height = (currHeight + 40) + "px";
-            myImg.style.width = (currWidth + 20) + "px";
+            myImg.style.height = (currHeight + 15) + "px";
+            myImg.style.width = (currWidth + 10) + "px";
 		} 
 }
 
-setInterval("zoomin()",1000);
-
 function reinit(){
-    player_vie = 100;
+    
+    
+
     VieMechant = 100;
     var currHeight = myImg.clientHeight;
     var currWidth = myImg.clientWidth;
@@ -139,6 +159,27 @@ function reinit(){
         affichageviemechant.innerHTML=vieMechant + " %"
 }
 
+function reinit2(){
+    
+    nbrVie--;
+    nbVie.innerHTML=nbrVie;
+
+    player_vie = 100;
+    VieMechant = 100;
+    var currHeight = myImg.clientHeight;
+    var currWidth = myImg.clientWidth;
+    var debHeight = 252;
+    var debWidth = 200;
+        myImg.style.height= (debHeight) + "px"
+        myImg.style.width= (debWidth) + "px"
+        myImg.style.display="flex";
+        myImg.style.justifyContent="center";
+        myImg.style.width= "40px";
+        myImg.style.height= "45px" ;  
+        changePerso()
+        vieMechant = 100;
+        affichageviemechant.innerHTML=vieMechant + " %"
+}
 
 function getRandomMechant(){
       var nbre = '123';
